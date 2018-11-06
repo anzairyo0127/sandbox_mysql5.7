c7d512bfa5dea0a61aec9a3155b9fa68a292a517
Ryo Anzai <36684885+anzairyo0127@users.noreply.github.com>
2018-11-06 22:18:45 +0900

    add class size

--- gomibako/ruby-book/test/taiyaki_test.py
+++ gomibako/ruby-book/test/taiyaki_test.py
@@ -7,17 +7,21 @@ import unittest
 
 
 class Taiyaki:
-    def __init__(self, anko):
+    def __init__(self, anko, size):
         self.anko = anko
+        self.size = size
 
 
 class TaiyakiTest(unittest.TestCase):
 
     def test_taiyaki(self):
-        taiyaki_1 = Taiyaki('あんこ')
+        taiyaki_1 = Taiyaki('あんこ', 'ふつう')
         self.assertEqual('あんこ', taiyaki_1.anko)
-        taiyaki_2 = Taiyaki('白あん')
+        self.assertEqual('ふつう', taiyaki_1.size)
+
+        taiyaki_2 = Taiyaki('白あん', '大きい')
         self.assertEqual('白あん', taiyaki_2.anko)
+        self.assertEqual('大きい', taiyaki_2.size)
 
 
 if __name__ == '__main__':
